11a76378e60869bae4d83d5910418409a0098c8f
linwenying <453535072@qq.com>
2017-01-03 11:14:41 +0800

    创建基本的文件夹目录

---
+++ src/main/java/com.lwy/entity/Users.java
@@ -0,0 +1,25 @@
+package com.lwy.entity;
+
+/**
+ * Created by lin on 2017/1/3.
+ */
+public class Users {
+    private int id;
+    private  String name;
+
+    public int getId() {
+        return id;
+    }
+
+    public void setId(int id) {
+        this.id = id;
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
+}
---
+++ src/test/java/com/lwy/UsersTest.java
@@ -0,0 +1,7 @@
+package com.lwy;
+
+/**
+ * Created by Administrator on 2017/1/3.
+ */
+public class UsersTest {
+}
